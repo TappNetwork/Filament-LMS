a4835c2c63f9ff64ea27013aa3f024a470af2f91
Steve Williamson <steve@sjdub.com>
2024-12-18 15:47:39 -0500

    tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,25 @@
 namespace Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Tapp\FilamentLms\FilamentLmsServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            FilamentLmsServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration->up();
+        */
+    }
 }

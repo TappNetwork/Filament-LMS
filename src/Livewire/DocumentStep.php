5c40944da7e195002541a9ba608cc5dfb5ead332
scottgrayson <7796074+scottgrayson@users.noreply.github.com>
2025-02-03 18:09:04 +0000

    Fix styling

--- src/Livewire/DocumentStep.php
+++ src/Livewire/DocumentStep.php
@@ -2,10 +2,9 @@
 
 namespace Tapp\FilamentLms\Livewire;
 
-use Livewire\Attributes\On;
 use Livewire\Component;
-use Tapp\FilamentLms\Models\Step;
 use Tapp\FilamentLms\Models\Document;
+use Tapp\FilamentLms\Models\Step;
 
 class DocumentStep extends Component
 {

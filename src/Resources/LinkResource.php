ea38bee7fb327e1acc5c9315afbe1ef223715971
johnwesely <29612767+johnwesely@users.noreply.github.com>
2025-02-04 20:25:20 +0000

    Fix styling

--- src/Resources/LinkResource.php
+++ src/Resources/LinkResource.php
@@ -2,11 +2,11 @@
 
 namespace Tapp\FilamentLms\Resources;
 
+use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Filament\Forms;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Tapp\FilamentLms\Models\Link;

dd49f1898a7232fb08d22155c9b6467caaa41712
John Wesely <john.wesely@tappnetwork.com>
2025-02-04 13:24:57 -0700

    include forms

--- src/Resources/LinkResource.php
+++ src/Resources/LinkResource.php
@@ -6,6 +6,7 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Forms;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Tapp\FilamentLms\Models\Link;

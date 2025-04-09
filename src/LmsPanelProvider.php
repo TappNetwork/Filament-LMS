803a9a657e88bcd7e3e1314ecd01b35cacc0f7f2
scottgrayson <7796074+scottgrayson@users.noreply.github.com>
2025-04-09 18:07:51 +0000

    Fix styling

--- src/LmsPanelProvider.php
+++ src/LmsPanelProvider.php
@@ -47,14 +47,13 @@ class LmsPanelProvider extends PanelProvider
 
         if (config('filament-lms.brand_logo')) {
             $panel->brandLogo(asset(config('filament-lms.brand_logo')));
-            
+
             if (config('filament-lms.brand_logo_height')) {
                 $panel->brandLogoHeight(config('filament-lms.brand_logo_height'));
             }
         } else {
             $panel->brandName(config('filament-lms.brand_name'));
         }
-
 
         return $panel
             ->id('lms')

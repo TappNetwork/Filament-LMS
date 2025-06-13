743f5bdeec2731eaa4b10cd13d2e803cd97844bf
scott grayson <Scott Grayson>
2025-06-13 15:43:50 -0400

    lms user trait

--- src/Traits/FilamentLmsUser.php
+++ src/Traits/FilamentLmsUser.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Tapp\FilamentLms\Models\Course;
 use Tapp\FilamentLms\Models\Step;
 
-trait HasLmsCourses
+trait FilamentLmsUser
 {
     /**
      * Get all courses the user is enrolled in.

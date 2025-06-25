266c4113a9fad492ee667b085c047b4db33e0aa4
Steve Williamson <steve@sjdub.com>
2025-06-25 12:52:19 -0400

    make course image cards more square

--- resources/views/components/course-card.blade.php
+++ resources/views/components/course-card.blade.php
@@ -1,6 +1,6 @@
-  <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white">
-    <img src="{{ $course->image_url }}" class="aspect-[3/4] w-full bg-gray-200 object-cover group-hover:opacity-75 sm:aspect-auto sm:h-96">
-    <div class="flex flex-1 flex-col space-y-2 p-4">
+  <div class="flex overflow-hidden relative flex-col bg-white rounded-lg border border-gray-200 group">
+    <img src="{{ $course->image_url }}" class="aspect-[3/4] w-full bg-gray-200 object-cover group-hover:opacity-75 sm:aspect-auto">
+    <div class="flex flex-col flex-1 p-4 space-y-2">
       <h3 class="text-sm font-medium text-gray-900">
         <a href="{{ $course->linkToCurrentStep() }}">
           <span aria-hidden="true" class="absolute inset-0"></span>

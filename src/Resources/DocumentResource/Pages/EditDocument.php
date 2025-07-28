2110333502524181d34c126417ec4c9314783734
scottgrayson <7796074+scottgrayson@users.noreply.github.com>
2025-07-28 16:36:24 +0000

    Fix styling

--- src/Resources/DocumentResource/Pages/EditDocument.php
+++ src/Resources/DocumentResource/Pages/EditDocument.php
@@ -5,7 +5,6 @@ namespace Tapp\FilamentLms\Resources\DocumentResource\Pages;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 use Tapp\FilamentLms\Resources\DocumentResource;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class EditDocument extends EditRecord
 {
@@ -24,8 +23,8 @@ class EditDocument extends EditRecord
     {
         $record = $this->record;
         $data = $this->data;
-        
-        if ($record && isset($data['media_name']) && !empty($data['media_name'])) {
+
+        if ($record && isset($data['media_name']) && ! empty($data['media_name'])) {
             $media = $record->getFirstMedia();
             if ($media) {
                 $media->update(['name' => $data['media_name']]);
@@ -36,14 +35,14 @@ class EditDocument extends EditRecord
     protected function mutateFormDataBeforeFill(array $data): array
     {
         $record = $this->record;
-        
+
         if ($record) {
             $media = $record->getFirstMedia();
             if ($media) {
                 $data['media_name'] = $media->name;
             }
         }
-        
+
         return $data;
     }
 }
